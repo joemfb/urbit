4dd3c217e2f162afcffc22a551641567b4c9c390
Joseph Bryan <joemfb@gmail.com>
2017-03-12 23:25:42 -0400

    pushes forks outside of cells

--- jets/f/cell.c
+++ jets/f/cell.c
@@ -12,9 +12,46 @@
   {
     if ( (c3__void == hed) || (c3__void == tal) ) {
       return c3__void;
-    } else {
-      return u3nt(c3__cell, u3k(hed), u3k(tal));
     }
+
+    if ( ( (c3y == u3du(hed)) && (c3__fork == u3h(hed)) ) ||
+         ( (c3y == u3du(tal)) && (c3__fork == u3h(tal)) ) ) {
+      u3_noun lit = u3_nul;
+
+      if ( (c3y != u3du(tal)) || (c3__fork != u3h(tal)) ) {
+        u3_noun hed_l = u3qdi_tap(u3t(hed), u3_nul);
+
+        while ( hed_l != u3_nul ) {
+          lit = u3nc(u3nt(c3__cell, u3k(u3h(hed_l)), u3k(tal)), lit);
+          hed_l = u3t(hed_l);
+        }
+      } else if ( (c3y != u3du(hed)) || (c3__fork != u3h(hed)) ) {
+        u3_noun tal_l = u3qdi_tap(u3t(tal), u3_nul);
+
+        while ( tal_l != u3_nul ) {
+          lit = u3nc(u3nt(c3__cell, u3k(hed), u3k(u3h(tal_l))), lit);
+          tal_l = u3t(tal_l);
+        }
+      } else {
+        u3_noun hed_l = u3qdi_tap(u3t(hed), u3_nul);
+        u3_noun ttal_l = u3qdi_tap(u3t(tal), u3_nul);
+
+        while ( hed_l != u3_nul ) {
+          u3_noun tal_l = ttal_l;
+
+          while ( tal_l != u3_nul ) {
+            lit = u3nc(u3nt(c3__cell, u3k(u3h(hed_l)), u3k(u3h(tal_l))), lit);
+            tal_l = u3t(tal_l);
+          }
+
+          hed_l = u3t(hed_l);
+        }
+      }
+
+      return u3qf_fork(lit);
+    }
+
+    return u3nt(c3__cell, u3k(hed), u3k(tal));
   }
   u3_noun
   u3wf_cell(u3_noun cor)
